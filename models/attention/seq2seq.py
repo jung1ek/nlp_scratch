42e518ec8416b41390f7e1c80da12a5c534ef2c7
jung1ek <tekjungsingh659@gmail.com>
2025-07-07 23:38:52 +0545

    s state

--- models/attention/seq2seq.py
+++ models/attention/seq2seq.py
@@ -31,9 +31,9 @@ class Encoder(nn.Module):
         b_h = b_h0
         f_output = []
         b_output = []
-        for i in range(seq_len):
-            f_h = torch.tanh((self.fw_hh@f_h+self.fb_hh) + (self.fw_ih@x[i]+self.fb_ih))
-            b_h = torch.tanh((self.bw_hh@b_h+self.bb_hh) + (self.bw_ih@x[-1-i]+self.bb_ih))
+        for j in range(seq_len):
+            f_h = torch.tanh((self.fw_hh@f_h+self.fb_hh) + (self.fw_ih@x[j]+self.fb_ih))
+            b_h = torch.tanh((self.bw_hh@b_h+self.bb_hh) + (self.bw_ih@x[-1-j]+self.bb_ih))
             
             f_output.append(f_h)
             b_output.append(b_h)
@@ -43,10 +43,31 @@ class Encoder(nn.Module):
 
 class Decoder(nn.Module):
 
-    def __init__(self,):
+    def __init__(self,input_size,hidden_size):
         super().__init__()
+        self.hidden_size = hidden_size
+
+        # decoder hidden weight, for calc e.
+        self.w = nn.Parameter(torch.randn(hidden_size,hidden_size))
+
+        # encoder's hidden weight, for calc e.
+        self.u = nn.Parameter(torch.randn(self.hidden_size,self.hidden_size*2))
+
+        # weghts for decoder calculating hidden value (s)
+        self.w_ih = nn.Parameter(torch.randn(hidden_size,input_size))
+        self.w_hh = nn.Parameter(torch.randn(hidden_size,hidden_size))
+        self.w_ch = nn.Parameter(torch.randn(hidden_size,hidden_size))
+
 
     def forward(self,enc_h,y):
         seq_len,_= y.shape
-        s = torch.zeros()
+        s = torch.zeros(self.hidden_size)
+
+        # calculating attention, and making each input scalar. (seq,h_enc) = seq,
+        # eij refers to for each i in output seq, every j (input seq), matrix correlation of each input with output
+        e = None
+        for i in range(seq_len):
+            pass
+
+
         
